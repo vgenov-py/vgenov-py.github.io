1648dcee7ec7a05f385662387f9890dc89f2ad16
vito <genovese.work@gmail.com>
2021-02-10 20:58:11 +0100

    Error Handeling Added

--- numbers.js
+++ numbers.js
@@ -1,3 +1,6 @@
+//MADE BY VITO GENOVESE
+//GITHUB: https://github.com/vgenov-py/
+
 const numToGuess = () => {
     const makeARandom = document.querySelector("#makeARandom");
     const numberStatus = document.querySelector("#numberStatus");
@@ -36,9 +39,11 @@ const instructions = document.querySelector("#instructionsButton");
 instructions.addEventListener("click", showInstructions);
 
 const guess = () => {
+    document.querySelector("#numberStatus").innerText = "";
     const pc = [...document.querySelector("#numToGuess").value];
     const user = document.querySelector("#user").value;
     let array = [...user];
+
     const winMessage = document.querySelector("#winMessage");
     const times = parseInt(winMessage.innerText) + 1;
 
@@ -46,29 +51,48 @@ const guess = () => {
     let exacts = 0;
     let missPlace = 0;
     let count = 0;
+    let passed = false;
     //LIST PREPEND:
     const list = document.querySelector("#list");
     //^^^^^
-    array.forEach((n) => {
-        if (n === pc[count]) {
-            exacts++;
-        } else if (pc.includes(n)) {
-            missPlace++;
+
+    const find = (array) => {
+        array.forEach((num) => {
+            if (array.includes(num, array.indexOf(num) + 1)) {
+                passed = true;
+            }
+        });
+        return passed;
+    };
+    find(array);
+    if (array.includes("0")) {
+        alert("¡No puedes incluir 0!");
+    } else if (array.length < 4 || array.length > 4) {
+        alert("¡El número debe ser de 4 cifras!");
+    } else if (passed) {
+        alert("¡No puedes repetir cifras!");
+    } else {
+        array.forEach((n) => {
+            if (n === pc[count]) {
+                exacts++;
+            } else if (pc.includes(n)) {
+                missPlace++;
+            }
+            count++;
+        });
+
+        winMessage.innerText = times;
+        document.querySelector("#user").value = "";
+        if (exacts === 4) {
+            winMessage.style.display = "";
+
+            winMessage.innerText = `¡Has ganado en ${times} intentos!`;
+            document.querySelector("#game").style.display = "none";
         }
-        count++;
-    });
-
-    winMessage.innerText = times;
-    document.querySelector("#user").value = "";
-    if (exacts === 4) {
-        winMessage.style.display = "";
-
-        winMessage.innerText = `¡Has ganado en ${times} intentos!`;
-        document.querySelector("#game").style.display = "none";
+        const li = document.createElement("li");
+        li.innerText = `${user} ${missPlace}M & ${exacts}E`;
+        list.prepend(li);
     }
-    const li = document.createElement("li");
-    li.innerText = `${user} ${missPlace}M & ${exacts}E`;
-    list.prepend(li);
 };
 const user = document.querySelector("#user");
 user.addEventListener("keydown", (e) => {
